73c61d5143d964ca83e9f0a08db7e59ec7850200
赵仝 <1427730623@qq.com>
2023-02-06 19:34:48 +0800

    工程初始化

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,52 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Greet from "./components/Greet.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h1>Welcome to Tauri!</h1>
+
+    <div class="row">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
+      </a>
+      <a href="https://tauri.app" target="_blank">
+        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+
+    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
+
+    <p>
+      Recommended IDE setup:
+      <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
+      +
+      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+      +
+      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
+        >Tauri</a
+      >
+      +
+      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
+        >rust-analyzer</a
+      >
+    </p>
+
+    <Greet />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+.logo.vite:hover {
+  filter: drop-shadow(0 0 2em #747bff);
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
+
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  filter: drop-shadow(0 0 2em #249b73);
 }
 </style>
