4d8360edff0a6ecf8abf231b710ba2de6b1aad17
zhaotong <1427730623@qq.com>
2024-07-14 01:11:41 +0800

    react ui初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
-
-import { type CSSObject, Global } from '@emotion/react';
-import { createAppTheme, createAppStylesBaseline,Text } from '@arwes/react';
-
-const theme = createAppTheme();
-const stylesBaseline = createAppStylesBaseline(theme);
-
+import { invoke } from "@tauri-apps/api/tauri";
+import { ModeToggle } from "@/components/mode-toggle";
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
@@ -19,18 +12,41 @@ function App() {
   }
 
   return (
-      <>
-          <Global styles={stylesBaseline as Record<string, CSSObject>} />
-          <Text style={{ color: '#ddd' }}>
-              Nemo enim ipsam <b>voluptatem quia voluptas</b> sit aspernatur
-              aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia
-              dolor sit amet, consectetur, <b>adipisci velit, <i>sed quia non</i></b>
-              numquam eius modi tempora incidunt ut labore et dolore magnam
-              <span><a href='#'>aliquam quaerat</a></span> voluptatem. Ut enim ad minima
-              veniam, qui nostrum exercitationem ullam corporis suscipit.
-          </Text>
-      </>
+    <div className="container bg-green-300 dark:bg-slate-600">
+      <h1 className="text-red-400 dark:text-green-300">Welcome to Tauri!</h1>
+
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+        <ModeToggle />
+      </form>
+
+      <p>{greetMsg}</p>
+    </div>
   );
 }
 
